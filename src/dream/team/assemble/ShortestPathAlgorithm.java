c40b582b5b123547cad78530b0f1efc8d8d76e1a
Cal <cmartin7@tcd.ie>
2015-11-24 13:12:11 +0000

    First Attempt At Shortest Path

--- src/dream/team/assemble/ShortestPathAlgorithm.java
+++ src/dream/team/assemble/ShortestPathAlgorithm.java
@@ -1,15 +1,44 @@
 package dream.team.assemble;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Cal
- * 
+ *
  * Dijkstra’s Shortest-Path Algorithm (with a sprinkle of Cal Magic)
  */
 public class ShortestPathAlgorithm
 {
-    public static RoutingTable getRoutingTable()
+
+    public static RoutingTable getRoutingTable(Node startNode)
     {
+        RoutingTable routingTable = new RoutingTable();
+        int totalWeight = 0;
+
+        ArrayList<Link> tentitiveList = new ArrayList<>();
+
+        routingTable.addEntry(startNode.myIP, startNode);
         
+        do
+        {
+            tentitiveList.addAll(startNode.connections);
+            
+            Link minimum = new Link(null, null, Integer.MAX_VALUE);
+            for (Link link : tentitiveList)
+            {
+                if (link.weight < minimum.weight)
+                {
+                    minimum = link;
+                }
+            }
+            
+            totalWeight += minimum.weight;
+            routingTable.addEntry(minimum.getConnection(startNode).myIP, minimum.getConnection(startNode));
+            startNode = minimum.getConnection(startNode);
+            
+        } while (!tentitiveList.isEmpty());
+
+        return routingTable;
     }
 }
