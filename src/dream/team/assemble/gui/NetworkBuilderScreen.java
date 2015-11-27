8448035660bb13f3e24156afc3db21a02f92fe1f
Cal <cmartin7@tcd.ie>
2015-11-27 13:14:13 +0000

    Adding freaking PHYSICS! :D

--- src/dream/team/assemble/gui/NetworkBuilderScreen.java
+++ src/dream/team/assemble/gui/NetworkBuilderScreen.java
@@ -68,23 +68,23 @@ public class NetworkBuilderScreen extends Screen
                 while (nodeList.isEmpty())
                 {
                     int amount = (int) gui.random(10);
-                    float radius = 300;
+                    float radius = (gui.width - 200) / 4;
                     float angle = RoutingGUI.TWO_PI / (float) amount;
                     for (int i = 0; i < amount; i++)
                     {
                         addNewNode((int) (radius * RoutingGUI.sin(angle * i)) + gui.width / 2, (int) (radius * RoutingGUI.cos(angle * i)) + gui.height / 2);
                     }
-                    for (Node node : nodeList)
+                    nodeList.stream().forEach((node) ->
                     {
-                        for (Node node2 : nodeList)
+                        nodeList.stream().filter((node2) -> (node != node2 && (int) gui.random(5) == 0)).map((node2) ->
                         {
-                            if (node != node2 && (int) gui.random(5) == 0)
-                            {
-                                node.addLinkedNode(node2);
-                                node2.addLinkedNode(node);
-                            }
-                        }
-                    }
+                            node.addLinkedNode(node2);
+                            return node2;
+                        }).forEach((node2) ->
+                        {
+                            node2.addLinkedNode(node);
+                        });
+                    });
                     for (int i = 0; i < nodeList.size(); i++)
                     {
                         Node node = nodeList.get(i);
@@ -116,6 +116,15 @@ public class NetworkBuilderScreen extends Screen
             }
         };
     }
+
+    @Override
+    void draw(RoutingGUI gui)
+    {
+        super.draw(gui);
+        updateNodePositions();
+    }
+    
+    
 
     public void dragNode(Node node, Node parent)
     {
@@ -187,4 +196,80 @@ public class NetworkBuilderScreen extends Screen
         });
         nodeList.add(tmpNode);
     }
+    
+     float distanceSqr(float dx, float dy) { 
+      return dx * dx + dy * dy; 
+    }
+
+    Force force(Node nodeA, Node nodeB, int force) {
+      float dx = nodeA.x - nodeB.x;
+      float dy = nodeA.y - nodeB.y;
+      float angle = (float) Math.atan2(dy, dx);
+      float ds = 0;
+      switch (force)
+      {
+          case 0:
+              ds = repelForce(distanceSqr(dx, dy));
+              break;
+          case 1:
+              ds = attractForce(distanceSqr(dx, dy));
+              break;
+          case 2:
+              ds = gravityForce(distanceSqr(dx, dy));
+              break;
+      }
+      return new Force((float) Math.cos(angle) * ds, (float) Math.sin(angle) * ds);
+    }
+
+    float repelForce(float distanceSqr) {
+      return 160000.0f / distanceSqr;
+    }
+
+    float  attractForce(float distanceSqr) {
+      return -distanceSqr / 20000.0f;
+    }
+
+    float gravityForce(float distanceSqr) {
+      return (float) (-Math.sqrt(distanceSqr) / 40.0f);
+    }
+
+
+    Force[] calculateForces() {
+      Force[] forces = new Force[nodeList.size()];  
+      for (int i = 0; i < nodeList.size(); i++) {
+        forces[i] = new Force(0,0);
+
+        // repelling between nodes:
+        for (int j = 0; j < nodeList.size(); j++) {
+          if (i == j)
+            continue;
+          Force f = force(nodeList.get(i), nodeList.get(j), 0);
+          forces[i].x += f.x;
+          forces[i].y += f.y;
+        }
+
+        // attraction between connected nodes:
+        for (int j = 0; j < nodeList.get(i).linkedNodes.size(); j++) {
+          Force f = force(nodeList.get(i), nodeList.get(i).linkedNodes.get(j), 1);
+          forces[i].x += f.x;
+          forces[i].y += f.y;          
+        }          
+
+        // gravity:
+        //var center = { x: 400, y: 300 };
+        Node center = new Node(gui.width / 2, gui.height / 2, 0, 0, "Center");
+        Force f = force(nodeList.get(i), center, 2);
+        forces[i].x += f.x;
+        forces[i].y += f.y;           
+      }  
+      return forces;
+    }
+
+    void updateNodePositions() {
+      Force[] forces = calculateForces();
+      for (int i = 0; i < forces.length; i++) {
+        nodeList.get(i).x += forces[i].x;      
+        nodeList.get(i).y += forces[i].y;           
+      }  
+    }    
 }
