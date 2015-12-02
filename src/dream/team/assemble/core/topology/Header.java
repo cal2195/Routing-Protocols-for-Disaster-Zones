6888c2d35ce730d6c2484b1070f82d4c9e51f3b3
dcrawfordtcd <crawford@tcd.ie>
2015-12-02 20:22:56 +0000

    More useful/fun methods added to the header class

--- src/dream/team/assemble/core/topology/Header.java
+++ src/dream/team/assemble/core/topology/Header.java
@@ -11,6 +11,14 @@ public class Header {
     private final byte[] sourceIP;
     private final byte[] destinationIP;
     
+    /**
+     *
+     * @param flags
+     * @param sourceIP
+     * @param destinationIP
+     */
+    
+    
     public Header(byte flags, String sourceIP, String destinationIP)
     {
         this.flags = flags;
@@ -30,7 +38,7 @@ public class Header {
         return temp;
     }
 
-        private String bytesToString(byte[] IP)
+    private String bytesToString(byte[] IP)
     {
         String temp = "";
         for(int i = 0; i < IP.length;i++)
@@ -41,18 +49,55 @@ public class Header {
         }
         return temp;
     }
+    
+    public boolean isFlagSet(int i)
+    {
+        byte temp = 1;
+        temp = (byte) (temp << i);
+        return (temp & flags) > 0;
+    }
 
+    public String flagString()
+    {
+        String temp = "";
+        byte checker = 1;
+        byte flagByte = flags;
+        for(int i = 0; i < 8; i++)
+        {
+            if(i == 4)
+                temp = " " + temp;
+            
+            if((checker & flagByte) > 0)
+                temp = "1" + temp;
+            else 
+                temp = "0" + temp;
+            flagByte = (byte) (flagByte >> 1);
+        }
+        return temp;
+    }
     
     @Override
     public String toString()
     {
-       return flags + " src: " + bytesToString(sourceIP) + " dst: " + bytesToString(destinationIP); 
+       return "" + flagString() + " src: " + bytesToString(sourceIP) + " dst: " + bytesToString(destinationIP); 
     }
     
     public static void main(String[] args)
     {
         Header testHeader = new Header((byte)7, "255.12.1.255", "192.168.1.1");
         System.out.println(testHeader);
+        for(int i = 0; i < 8; i++)
+        {
+            System.out.println(testHeader.isFlagSet(i));
+        }
+        
+        testHeader = new Header((byte)32, "1.254.6.24", "1.250.25.1");
+        System.out.println(testHeader);
+        for(int i = 0; i < 8; i++)
+        {
+            System.out.println(testHeader.isFlagSet(i));
+        }
+        
     }
     
 }
