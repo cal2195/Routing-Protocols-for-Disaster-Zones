ea783373a063c59d18b83ed86e07e7a0798fdd53
dcrawfordtcd <crawford@tcd.ie>
2015-12-08 10:14:29 +0000

    Added arraylist to store basic log of messages received and routed, added boolean for router logfiles

--- src/dream/team/assemble/core/AbstractRouter.java
+++ src/dream/team/assemble/core/AbstractRouter.java
@@ -1,5 +1,10 @@
 package dream.team.assemble.core;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,15 +17,21 @@ import java.util.Map;
 public abstract class AbstractRouter
 {
     private final String localIP;
+    private ArrayList<String> log;
+    private final boolean logToFile = false;
+    private PrintWriter logFile = null;
     
     /**
      * Maps destination address to next hop address.
      */
     private final Map<String, String> routingTable = new HashMap<>(); // NOTE: Consider changing Map to RangeMap, for IP ranged lookup.
     
-    public AbstractRouter(String ip)
+    public AbstractRouter(String ip) throws FileNotFoundException, UnsupportedEncodingException
     {
         this.localIP = ip;
+        if(logToFile)
+            logFile = new PrintWriter(localIP, "UTF-8");
+
     }
     
     public String getAddress()
@@ -36,12 +47,14 @@ public abstract class AbstractRouter
     public void onReceipt(byte[] data)
     {
         RouterPacket packet = new RouterPacket(data);
+        String logString = packet.toString();
         
         String dstAddr = packet.getDstAddr();
         
         /* if addressed for this AbstractRouter then handle it as is appropriate for packet type */
         if (localIP.equals(dstAddr))
         {
+            logString += new String(packet.getPayload());
             // packet handling stuff goes here
             
             // TEMPORARY -->
@@ -51,9 +64,15 @@ public abstract class AbstractRouter
         /* if addressed for another node then pass to adderss of next hop */
         else 
         {
+
             String nextAddr = routingTable.get(dstAddr);
+            logString += " - routed to " + nextAddr;
             send(data, nextAddr);
         }
+        
+        log.add(logString);
+        if(logToFile)
+            logFile.write(logString);
         
     }
     
